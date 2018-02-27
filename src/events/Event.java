9c8ed567ea33e1df9d25c7091a3295277df8aa15
Oscar Brink <oscar_brink95@msn.com>
2018-02-27 10:38:51 +0100

    done basic FIFO + spaces -> tabs

--- src/events/Event.java
+++ src/events/Event.java
@@ -1,5 +1,9 @@
 package events;
 
 public abstract class Event {
+	private int time;
 
+	public int getTime() {
+		return time;
+	}
 }
--- src/events/EventQueue.java
+++ src/events/EventQueue.java
@@ -1,11 +1,48 @@
 package events;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class EventQueue {
 
-    private ArrayList<Event> queueList = new ArrayList<>();
+	private ArrayList<Event> queueList = new ArrayList<>();
 
-    
+	/**
+	 * Adds a new item at the end of the queue.
+	 *
+	 * @param   event   Event to be added.
+	 */
+	public void addEvent(Event event) {
+		queueList.add(event);
+	}
+
+	/**
+	 * Removes the first object from the queue.
+	 */
+	public void removeFirst() throws NoSuchElementException {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
+		queueList.remove(0);
+	}
+
+	/**
+	 * Gets the first object in the queue.
+	 */
+	public Object getFirst() throws NoSuchElementException {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return queueList.get(0);
+	}
+
+	/**
+	 * Gets the current size of the queue.
+	 *
+	 * @return  int Current size.
+	 */
+	public int size() {
+		return queueList.size();
+	}
 
 }
