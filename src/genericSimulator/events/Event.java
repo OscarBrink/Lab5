ed01c5af40107e31f3b58f00ad74f03b2b5be7ec
Oscar Brink <oscar_brink95@msn.com>
2018-03-04 17:54:57 +0100

    commenting Event.java

--- src/genericSimulator/events/Event.java
+++ src/genericSimulator/events/Event.java
@@ -1,16 +1,33 @@
 package genericSimulator.events;
 
+/**
+ * Class implements an abstract Event that can be run during a simulation.
+ */
 public abstract class Event {
 	protected double time;
 
+	/**
+	 * @return double gets the time that the
+	 * Event is executed in the simulation.
+	 */
 	public double getTime() {
 		return time;
 	}
 
+	/**
+	 * @return String name of the Event.
+	 */
 	public abstract String getEventName();
 
+	/**
+	 * Executes the effect of the Event
+	 */
 	public abstract void effect();
 
+	/**
+	 * Gets a String array containing information about the Event.
+	 * @return returns an array with info about the Event.
+	 */
 	public abstract String[] getPrintInfo();
 
 }
