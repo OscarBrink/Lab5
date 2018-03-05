5ce13d5329ef320782ca6925503cf0032e4a4618
Marc Nilsson <marc.nilsson@gmail.com>
2018-03-05 14:55:21 +0100

    Optimize forts.

--- src/main/Mian.java
+++ src/main/Mian.java
@@ -3,28 +3,28 @@ package main;
 public class Mian {
 
 	public static void main(String[] args) {
-//		RunSimulator runSim = new RunSimulator(
+		RunSimulator runSim = new RunSimulator(
+				999.0,
+				1,
+				4.0,
+				5.0,
+				15.0,
+				20.0,
+				70.0,
+				1,
+				1
+		);
+		
+//		Optimize opti = new Optimize(
 //				999.0,
 //				5,
 //				1.0,
-//				0.5,
-//				1.0,
+//				5,
+//				15,
 //				2.0,
 //				3.0,
 //				1234,
-//				2
+//				1
 //		);
-		
-		Optimize opti = new Optimize(
-				999.0,
-				5,
-				1.0,
-				0.5,
-				1.0,
-				2.0,
-				3.0,
-				1234,
-				2
-		);
 	}
 }
--- src/main/Optimize.java
+++ src/main/Optimize.java
@@ -2,6 +2,7 @@ package main;
 
 import genericSimulator.*;
 import genericSimulator.events.EventQueue;
+import supermarketSimulator.supermarketEvents.CloseSupermarketEvent;
 import supermarketSimulator.supermarketEvents.StartEvent;
 import supermarketSimulator.supermarketEvents.StopSimEvent;
 import supermarketSimulator.supermarketState.SupermarketState;
@@ -12,11 +13,16 @@ public class Optimize {
 	SupermarketState state;
 	Simulator simulator;
 	boolean isRunning = false;
-	int numberOfOptimizations = 10;
-	int decreaseLimit = 10;
+	int configurationRuns = 10;
+	int maxMin = 99;
+	int maxMissedThreshold = 0;
+	int currentNumberOfMissedCustomers;
+	int maxNumberOfCashiers;
+	int tempCashiers;
 
 	public Optimize(double stopTime, int maxAmountCustomers, double lambda, double PickMin, double PickMax, double payMin, double payMax, long seed, int openCashiers) {
 
+		tempCashiers = openCashiers;
 		EventQueue eventQueue = new EventQueue();
 		state = new SupermarketState(eventQueue);
 		state.setMaxCustomers(maxAmountCustomers);
@@ -28,45 +34,43 @@ public class Optimize {
 		state.setPayMax(payMax);
 		state.setSeed(seed);
 
+		state.initTimeState();
+
 		eventQueue.addEvent(new StartEvent(0, state, eventQueue));
+		eventQueue.addEvent(new CloseSupermarketEvent(10.0, state, eventQueue));
 		eventQueue.addEvent(new StopSimEvent(stopTime, state));
 
 		SupermarketView view = new SupermarketView(state);
 
-		simulator = new Simulator(eventQueue, state, view);
+		simulator = new Simulator(eventQueue, state, view);		
 		
-		System.out.println(state.getEmergencyBreak());
-		//runOpti(openCashiers);
-		System.out.println(state.getEmergencyBreak());
-		
-		int i = 0;
-		while(i < numberOfOptimizations) {
-			//if(isRunning) {
-			//} else {
-				runOpti(openCashiers);
-				System.out.println(i);
-				i++;
-			//}
-		}
-		
+		optimizeCashiers(tempCashiers);
+
 	}
 	
-	void runOpti(int openCashiers) {
-		isRunning = true;
+	
+	
+	int runOpti(int tempCashiers) {
+		state.setOpenCashiers(tempCashiers);
 		simulator.runWithNoPrint();
-		while(true) {
-			if(state.getEmergencyBreak()) {
-				break;
-			}
-		}
-		System.out.println(state.getNumberOfMissedCustomers());
-		isRunning = false;
-		return;
+		System.out.println(this.state.getNumberOfMissedCustomers());	
+		return state.getNumberOfMissedCustomers();
 	}
 	
-	void optimizeCashiers() {
-		int numberOfRunsWithoutDecreasing = 0;
-		while(numberOfRunsWithoutDecreasing)
+	int optimizeCashiers(int tempCashiers) {
+			int runsWithoutADecrease = 0;
+
+			while(maxMin > 0) {
+				while(runsWithoutADecrease < configurationRuns) {		
+				currentNumberOfMissedCustomers = runOpti(tempCashiers);
+				System.out.println(currentNumberOfMissedCustomers);
+					if(currentNumberOfMissedCustomers < maxMin) {
+						maxMin = currentNumberOfMissedCustomers;			
+					} else if(currentNumberOfMissedCustomers == maxMin) {
+						runsWithoutADecrease++;
+					}
+				} 	
+			} return maxMin;
 	}
 
 }
