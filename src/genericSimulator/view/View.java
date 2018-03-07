27f85c67a052a9ec624bc2d871991fc9e40bbe9e
Oscar Brink <oscar_brink95@msn.com>
2018-03-07 16:35:36 +0100

    View cleaned and commented

--- src/genericSimulator/view/View.java
+++ src/genericSimulator/view/View.java
@@ -3,8 +3,13 @@ package genericSimulator.view;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * Implements a generic view to provide output for the user of the
+ * simulation.
+ */
 public abstract class View implements Observer{
 
+	@Override
 	public abstract void update(Observable arg0, Object arg1);
 
 }
