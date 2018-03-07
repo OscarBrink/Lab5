31093b673b1fe5bfbd4c593de8c2e62b5340f2ca
Oscar Brink <oscar_brink95@msn.com>
2018-03-07 16:47:13 +0100

    Simulator cleaned and commented

--- src/genericSimulator/Simulator.java
+++ src/genericSimulator/Simulator.java
@@ -4,13 +4,28 @@ import genericSimulator.events.EventQueue;
 import genericSimulator.state.State;
 import genericSimulator.view.View;
 
+/**
+ * Class implements a DEDS-Simulator that can be run with the use of
+ * specific States and Views.
+ *
+ * @author Josefine Bexelius
+ * @author Oscar Brink
+ * @author Lisa Jonsson
+ * @author Marc Nilsson
+ */
 public class Simulator {
 
 	private EventQueue eventQueue;
 	private State state;
 	private View view;
 
-
+	/**
+	 * Constructor.
+	 *
+	 * @param eventQueue	EventQueue that holds the Events.
+	 * @param state			State that tracks the current state.
+	 * @param view			View that provides output to user.
+	 */
 	public Simulator(EventQueue eventQueue, State state, View view) {
 		this.eventQueue = eventQueue;
 		this.state = state;
@@ -19,6 +34,9 @@ public class Simulator {
 		state.addObserver(view);
 	}
 
+	/**
+	 * Runs the simulation.
+	 */
 	public void run() {
 		while (!state.getEmergencyBreak()) {
 			state.updateState();
@@ -30,7 +48,10 @@ public class Simulator {
 			eventQueue.removeFirst();
 		}
 	}
-	
+
+	/**
+	 * Runs the simulation without updating the View.
+	 */
 	public void runWithNoPrint() {
 		while (!state.getEmergencyBreak()) {
 			eventQueue.getFirst().effect();
