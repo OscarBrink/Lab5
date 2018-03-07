a989341fc09c65035462414a2d65713b57021d08
Oscar Brink <oscar_brink95@msn.com>
2018-03-07 16:34:17 +0100

    State cleaned and commented

--- src/genericSimulator/state/State.java
+++ src/genericSimulator/state/State.java
@@ -2,23 +2,35 @@ package genericSimulator.state;
 
 import java.util.Observable;
 
+/**
+ * Class describes the current state of the simulation.
+ *
+ * @author Josefine Bexelius
+ * @author Oscar Brink
+ * @author Lisa Jonsson
+ * @author Marc Nilsson
+ */
 public abstract class State extends Observable {
 	private boolean emergencyBreak = false;
-	int currentTime;
 
+	/**
+	 * @return 	boolean that is true if simulation should break execution.
+	 * 			false otherwise.
+	 */
 	public boolean getEmergencyBreak() {
 		return emergencyBreak;
 	}
 
+	/**
+	 * Stops the simulation.
+	 */
 	public void stopSim() {
 		emergencyBreak = true;
 	}
 
-	@Override
-	public synchronized void setChanged() {
-		super.setChanged();
-	}
-
+	/**
+	 * Updates state for next Event in EventQueue.
+	 */
 	public abstract void updateState();
 
 }
